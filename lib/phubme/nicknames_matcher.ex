337d1ad764cac81144de89e471ac8dd5654cce2a
Benoit Tigeot <benoit@hopsandfork.com>
2016-08-30 15:35:27 +0200

    Manage none matching nicknames

--- lib/phubme/nicknames_matcher.ex
+++ lib/phubme/nicknames_matcher.ex
@@ -3,16 +3,20 @@ defmodule PhubMe.NicknamesMatcher do
     {:ok, full_comment, matching_nicknames(github_nicknames, [], 0), sender, comment_parsed }
   end
 
-  def match_nicknames(comment_parsed) do
+  def match_nicknames(_) do
     {:no_nicknames_found}
   end
 
-  defp matching_nicknames([h | t], matched_nicknames, array_position) do
-    matched_nicknames = List.insert_at(matched_nicknames, array_position, (h ++ nickname_from_mix_config(h)))
-    matching_nicknames(t, matched_nicknames, array_position + 1)
+  defp matching_nicknames([github_nick_head | github_nick_tail], matched_nicknames, array_position) do
+    if nickname_from_mix_config(github_nick_head) == :error do
+      IO.puts "No matching nickname found for " <> List.first(github_nick_head)
+    else
+      matched_nicknames = List.insert_at(matched_nicknames, array_position, (github_nick_head ++ nickname_from_mix_config(github_nick_head)))
+    end
+    matching_nicknames(github_nick_tail, matched_nicknames, array_position + 1)
   end
 
-  defp matching_nicknames([], matched_nicknames, array_position) do
+  defp matching_nicknames([], matched_nicknames, _) do
     matched_nicknames
   end
 
@@ -20,7 +24,7 @@ defmodule PhubMe.NicknamesMatcher do
     # IO.inspect Application.fetch_env(:phubme, String.to_atom(List.first(github_nick_head)))
     case Application.fetch_env(:phubme, String.to_atom(List.first(github_nick_head))) do
       {:ok, slack_nickname} -> [slack_nickname]
-      :error -> ""
+      :error -> :error
     end
   end
 end
--- test/lib/phubme/nicknames_matcher_test.exs
+++ test/lib/phubme/nicknames_matcher_test.exs
@@ -2,7 +2,6 @@ ExUnit.start
 
 defmodule NicknamesMatcher do
   use ExUnit.Case, async: true
-  import ExUnit.CaptureIO
 
   defp comment_with_nicknames, do: "Hey @HannahArrendt you should take a look at @lucie"
   defp tuples_with_matching_nicknames do
@@ -24,7 +23,7 @@ defmodule NicknamesMatcher do
 
     test "nicknames received with matching nickname" do
       assert PhubMe.NicknamesMatcher.match_nicknames(tuples_with_only_one_matching_nicknames) ==
-        {:ok, comment_with_nicknames, [["@Hannah", "@HannahSlack"], ["@luie"]], "baxterthehacker", "https://github.com/comment"}
+        {:ok, comment_with_nicknames, [["@Hannah", "@HannahSlack"]], "baxterthehacker", "https://github.com/comment"}
     end
   end
 end
