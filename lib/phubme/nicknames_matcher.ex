f11df96211d1fbf76fee97cd2c9479a43e2d7646
Nicolas ZERMATI <nicoolas25@gmail.com>
2016-09-05 16:07:42 +0200

    Update the code to show a refactoring of nicknames_matcher.ex
    This code is only for demonstrate a more idiomatic way of doing a
    first step in the recursion, before optimization.

--- lib/phubme/nicknames_matcher.ex
+++ lib/phubme/nicknames_matcher.ex
@@ -1,31 +1,27 @@
 defmodule PhubMe.NicknamesMatcher do
   def match_nicknames({full_comment, github_nicknames, sender, comment_parsed}) do
-    {:ok, full_comment, matching_nicknames(github_nicknames, [], 0), sender, comment_parsed }
+    {:ok, full_comment, matching_nicknames(github_nicknames), sender, comment_parsed }
   end
 
   def match_nicknames(_) do
     {:no_nicknames_found}
   end
 
-  defp matching_nicknames([github_nick_head | github_nick_tail], matched_nicknames, array_position) do
-    updated_matched_nicknames =
-      if nickname_from_mix_config(github_nick_head) == :error do
-        IO.puts "No matching nickname found for " <> List.first(github_nick_head)
-        matched_nicknames
-      else
-        List.insert_at(matched_nicknames, array_position, (github_nick_head ++ nickname_from_mix_config(github_nick_head)))
-      end
-    matching_nicknames(github_nick_tail, updated_matched_nicknames, array_position + 1)
-  end
-
-  defp matching_nicknames([], matched_nicknames, _) do
-    matched_nicknames
-  end
-
-  defp nickname_from_mix_config(github_nick_head) do
-    case Application.fetch_env(:phubme, String.to_atom(List.first(github_nick_head))) do
-      {:ok, slack_nickname} -> [slack_nickname]
-      :error -> :error
+  defp matching_nicknames([ [ nickname ] | tail]) do
+    case nickname_from_mix_config(nickname) do
+      {:ok, matching_nickname} ->
+        [ [ nickname, matching_nickname ] | matching_nicknames(tail) ]
+      :error ->
+        IO.puts "No matching nickname found for " <> nickname
+        matching_nicknames(tail)
     end
+  end
+
+  defp matching_nicknames([]) do
+    []
+  end
+
+  defp nickname_from_mix_config(nickname) do
+    Application.fetch_env(:phubme, String.to_atom(nickname))
   end
 end
