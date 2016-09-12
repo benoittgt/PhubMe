900be3d4a528602b00e44b5ef05de66723ec1ba8
Benoit Tigeot <btigeot@octo.com>
2016-09-12 22:04:30 +0200

    Add correct test behavior as written

--- test/lib/phubme/nicknames_matcher_test.exs
+++ test/lib/phubme/nicknames_matcher_test.exs
@@ -7,8 +7,8 @@ defmodule NicknamesMatcher do
   defp tuples_with_matching_nicknames do
     {comment_with_nicknames, [["@Hannah"], ["@lucie"]], "baxterthehacker", "https://github.com/comment"}
   end
-  defp tuples_with_only_one_matching_nicknames do
-    {comment_with_nicknames, [["@Hannah"], ["@luie"]], "baxterthehacker", "https://github.com/comment"}
+  defp tuples_with_no_matching_nicknames do
+    {comment_with_nicknames, [["@Hnnah"], ["@luie"]], "baxterthehacker", "https://github.com/comment"}
   end
 
   describe "PhubMe.NicknamesMatcher.match_nicknames/1" do
@@ -16,14 +16,14 @@ defmodule NicknamesMatcher do
       assert PhubMe.NicknamesMatcher.match_nicknames("stop here") ==  {:no_nicknames_found}
     end
 
-    test "nicknames received with no matching nickname" do
+    test "nicknames received with two matching nicknames" do
       assert PhubMe.NicknamesMatcher.match_nicknames(tuples_with_matching_nicknames) ==
         {:ok, comment_with_nicknames, [["@Hannah", "@HannahSlack"], ["@lucie", "@lulu"]], "baxterthehacker", "https://github.com/comment"}
     end
 
-    test "nicknames received with matching nickname" do
-      assert PhubMe.NicknamesMatcher.match_nicknames(tuples_with_only_one_matching_nicknames) ==
-        {:ok, comment_with_nicknames, [["@Hannah", "@HannahSlack"]], "baxterthehacker", "https://github.com/comment"}
+    test "nicknames received with no matching nickname" do
+      assert PhubMe.NicknamesMatcher.match_nicknames(tuples_with_no_matching_nicknames) ==
+        {:ok, comment_with_nicknames, [], "baxterthehacker", "https://github.com/comment"}
     end
   end
 end
