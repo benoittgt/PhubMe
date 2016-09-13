7fe3a2bd10d4be9d3d2c79a78893e62e44a76a89
Nicolas ZERMATI <nicoolas25@gmail.com>
2016-09-13 08:11:17 +0200

    Use an unique tuple when processing comments
    This refactoring is from the idea in #5 that the format of the list of
    nicknames was maybe too complex. After digging into this, I noticed
    another thing in the CommentParser: `process_comment` would act differently
    depending on if there is mentions or not.

--- lib/phubme/comment_parser.ex
+++ lib/phubme/comment_parser.ex
@@ -1,32 +1,15 @@
 defmodule PhubMe.CommentParser do
   def process_comment(body_params) do
     comment = get_in(body_params, ["comment", "body"])
-    sender  = get_in(body_params, ["comment", "user", "login"])
-    comment_source = get_in(body_params, ["comment", "html_url"])
-    IO.puts "Processing comment : \"" <> comment <> "\" from " <> sender
-    comment = comment
-              |> nicknames_present
-              |> extract_nicknames
-    case comment do
-      {:ok, full_comment, nicknames} -> {full_comment, nicknames, sender, comment_source}
-      {:error, _} -> "stop here"
-    end
+    sender = get_in(body_params, ["comment", "user", "login"])
+    source = get_in(body_params, ["comment", "html_url"])
+    nicknames = comment |> extract_nicknames
+    IO.puts "Processing comment : \"#{comment}\" from #{sender}"
+    {comment, nicknames, sender, source}
   end
 
-  defp nicknames_present(comment) do
-    if Regex.match?(~r{@([A-Za-z0-9_]+)}, comment) do
-      {:nicknames_found, comment}
-    else
-      {:no_nicknames_found, comment}
-    end
-  end
-
-  defp extract_nicknames({:nicknames_found, comment}) do
-    nicknames = Regex.scan ~r{@([A-Za-z0-9_]+)}, comment, capture: :first
-    {:ok, comment, nicknames}
-  end
-
-  defp extract_nicknames({:no_nicknames_found, _}) do
-    {:error, "No nicknames found in message"}
+  defp extract_nicknames(comment) do
+    matches = Regex.scan(~r{@([A-Za-z0-9_]+)}, comment, capture: :first)
+    matches |> List.flatten
   end
 end
--- test/lib/phubme/nicknames_matcher_test.exs
+++ test/lib/phubme/nicknames_matcher_test.exs
@@ -4,16 +4,20 @@ defmodule NicknamesMatcher do
   use ExUnit.Case, async: true
 
   defp comment_with_nicknames, do: "Hey @HannahArrendt you should take a look at @lucie"
+  defp comment_without_nickname, do: "Hello Hannah"
   defp tuples_with_matching_nicknames do
-    {comment_with_nicknames, [["@Hannah"], ["@lucie"]], "baxterthehacker", "https://github.com/comment"}
+    {comment_with_nicknames, ["@Hannah", "@lucie"], "baxterthehacker", "https://github.com/comment"}
   end
   defp tuples_with_no_matching_nicknames do
-    {comment_with_nicknames, [["@Hnnah"], ["@luie"]], "baxterthehacker", "https://github.com/comment"}
+    {comment_with_nicknames, ["@Hnnah", "@luie"], "baxterthehacker", "https://github.com/comment"}
+  end
+  defp tuples_with_no_nicknames do
+    {comment_without_nickname, [], "baxterthehacker", "https://github.com/comment"}
   end
 
   describe "PhubMe.NicknamesMatcher.match_nicknames/1" do
     test "no tuples received" do
-      assert PhubMe.NicknamesMatcher.match_nicknames("stop here") ==  {:no_nicknames_found}
+      assert PhubMe.NicknamesMatcher.match_nicknames(tuples_with_no_nicknames) ==  {:no_nicknames_found}
     end
 
     test "nicknames received with two matching nicknames" do
