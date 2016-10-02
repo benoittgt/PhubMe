cea767829b54d714f50f9f62d6d6d82e4c2760d4
Benoit Tigeot <btigeot@octo.com>
2016-10-02 07:25:49 +0200

    Refactor slack failing auth

--- lib/phubme/slack.ex
+++ lib/phubme/slack.ex
@@ -6,11 +6,9 @@ defmodule PhubMe.Slack do
 
   def send_private_message({ comment, [nick_head | nick_tail], sender, source}) do
     case Slack.Web.Chat.post_message(nick_head, formated_message(comment, sender, source), %{token: slack_token}) do
-      #  %{"error" => "invalid_auth", "ok" => false, "warning" => "superfluous_charset"}
-      %{"error" => "invalid_auth"} -> IO.puts "Invalide auth"
-      # %{"ok" => false} -> IO.puts "Not matching channel with the nickname " <> nick_head <> ". Are you sure it exists?"
-      x -> IO.puts "here is a list: #{inspect x}"
-      _ -> IO.puts "Matching channel found."
+      %{"error" => "invalid_auth"} -> invalid_slack_auth_message
+      %{"ok" => false} -> no_matching_channel_message(nick_head)
+      _ -> matching_channel_message
     end
     send_private_message({comment, nick_tail, sender, source})
   end
@@ -21,5 +19,18 @@ defmodule PhubMe.Slack do
 
   defp slack_token do
     Application.fetch_env!(:slack, :api_token)
+  end
+
+  defp invalid_slack_auth_message do
+    raise "Failed to connect. Are you sure you add correct PHUB_ME_SLACK_API_TOKEN?"
+  end
+
+  defp no_matching_channel_message(nick) do
+    IO.puts "Not matching channel with the nickname "
+        <> nick <> ". Are you sure it exists?"
+  end
+
+  defp matching_channel_message do
+    IO.puts "Matching channel found."
   end
 end
