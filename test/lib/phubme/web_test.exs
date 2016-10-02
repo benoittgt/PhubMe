71e440b1c24653a8b1a1c7ada9423a4c6d87e77d
Benoit Tigeot <btigeot@octo.com>
2016-10-02 07:36:36 +0200

    Add VCR tape for plug post test

--- test/lib/phubme/web_test.exs
+++ test/lib/phubme/web_test.exs
@@ -1,6 +1,7 @@
 defmodule PhubMe.WebTest do
   use ExUnit.Case, async: false
   use Plug.Test
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   @opts PhubMe.Web.init([])
   @github_issue_comment_request_json File.read!("test/fixtures/github_issue_comment_request.json")
@@ -9,16 +10,18 @@ defmodule PhubMe.WebTest do
    {"content-type", "application/json"}]
 
   test "returns ok when posting / with params" do
-    conn = conn(:post, "/phubme", @github_issue_comment_request_json)
-            |> put_req_header("x-github-event", "issue_comment")
-            |> put_req_header("content-type", "application/json")
-    conn = PhubMe.Web.call(conn, @opts)
+    use_cassette "slack channel found" do
+      conn = conn(:post, "/phubme", @github_issue_comment_request_json)
+             |> put_req_header("x-github-event", "issue_comment")
+             |> put_req_header("content-type", "application/json")
+      conn = PhubMe.Web.call(conn, @opts)
 
-    assert conn.state == :sent
-    assert conn.status == 200
-    assert conn.req_headers == @headers_expected
-    assert conn.params == Poison.decode! @github_issue_comment_request_json
-    assert conn.resp_body == "ok"
+      assert conn.state == :sent
+      assert conn.status == 200
+      assert conn.req_headers == @headers_expected
+      assert conn.params == Poison.decode! @github_issue_comment_request_json
+      assert conn.resp_body == "ok"
+    end
   end
 
   test "404 when wrong route" do
