09fefb2ce42df16c91523b51a3ed2036c4bb3bd1
Benoit Tigeot <btigeot@octo.com>
2017-02-19 15:59:38 +0100

    Separate how we process github payload + style changes

--- lib/phubme/web.ex
+++ lib/phubme/web.ex
@@ -12,21 +12,22 @@ defmodule PhubMe.Web do
   end
 
   def start_link do
-    adapeur_port = String.to_integer(System.get_env("PORT") || "8080")
-    {:ok, _ } = Plug.Adapters.Cowboy.http(PhubMe.Web, [], port: adapeur_port)
+    port = String.to_integer(System.get_env("PORT") || "8080")
+    {:ok, _ } = Plug.Adapters.Cowboy.http(PhubMe.Web, [], port: port)
   end
 
   post "/phubme" do
-      if valid_github_payload?(conn.body_params, conn.req_headers) do
-        conn
-        |> put_resp_content_type("application/json")
-        |> send_resp(200, "ok")
-        |> halt
-      else
-        conn
-        |> put_resp_content_type("application/json")
-        |> send_resp(404, "")
-        |> halt
+    if valid_github_payload?(conn.body_params, conn.req_headers) do
+      handle_github_payload(conn.body_params)
+      conn
+      |> put_resp_content_type("application/json")
+      |> send_resp(200, "ok")
+      |> halt
+    else
+      conn
+      |> put_resp_content_type("application/json")
+      |> send_resp(404, "")
+      |> halt
     end
   end
 
@@ -37,17 +38,17 @@ defmodule PhubMe.Web do
     |> halt
   end
 
-  def valid_github_payload?(%{"issue" => _issue} = body_params, [{"x-github-event", "issue_comment"}, __content_type]) do
+  defp valid_github_payload?(%{"issue" => _issue}, [{"x-github-event", "issue_comment"}, __content_type]), do: true
+  defp valid_github_payload?(%{"hook" => _hook}, [{"x-github-event", "ping"}, _content_type]), do: true
+  defp valid_github_payload?(_body_params, _req_headers), do: false
+
+  defp handle_github_payload(%{"issue" => _issue} = body_params) do
     PhubMe.CommentParser.process_comment(body_params)
     |> PhubMe.NicknamesMatcher.match_nicknames
     |> PhubMe.Slack.send_private_message
   end
 
-  def valid_github_payload?(%{"hook" => _hook} = _body_params, [{"x-github-event", "ping"}, _content_type]) do
-    true
-  end
-
-  def valid_github_payload?(_body_params, _req_headers) do
-    false
+  defp handle_github_payload(_body_params) do
+    :not_an_issue_comment
   end
 end
