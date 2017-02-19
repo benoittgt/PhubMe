1304cbeed6c783fc57f326866413b2bf3ee30aca
Benoit Tigeot <btigeot@octo.com>
2017-02-19 15:33:01 +0100

    Use pattern matching for valid_github_payload rather than cond

--- lib/phubme/web.ex
+++ lib/phubme/web.ex
@@ -24,13 +24,12 @@ defmodule PhubMe.Web do
   end
 
   post "/phubme" do
-    cond do
-      valid_github_payload?(conn.body_params, conn.req_headers) ->
+      if valid_github_payload?(conn.body_params, conn.req_headers) do
         conn
         |> put_resp_content_type("application/json")
         |> send_resp(200, "ok")
         |> halt
-      true ->
+      else
         conn
         |> put_resp_content_type("application/json")
         |> send_resp(404, "")
@@ -45,16 +44,17 @@ defmodule PhubMe.Web do
     |> halt
   end
 
-  def valid_github_payload?(body_params, req_headers) do
-    cond do
-      Map.has_key?(body_params, "issue") &&
-        req_headers == [{"x-github-event", "issue_comment"}, {"content-type", "application/json"}] ->
-        PhubMe.CommentParser.process_comment(body_params)
-        |> PhubMe.NicknamesMatcher.match_nicknames
-        |> PhubMe.Slack.send_private_message
-      Map.has_key?(body_params, "hook") &&
-        req_headers == [{"x-github-event", "ping"}, {"content-type", "application/json"}] -> true
-      true -> false
-    end
+  def valid_github_payload?(%{"issue" => _issue} = body_params, [{"x-github-event", "issue_comment"}, __content_type]) do
+    PhubMe.CommentParser.process_comment(body_params)
+    |> PhubMe.NicknamesMatcher.match_nicknames
+    |> PhubMe.Slack.send_private_message
+  end
+
+  def valid_github_payload?(%{"hook" => _hook} = _body_params, [{"x-github-event", "ping"}, _content_type]) do
+    true
+  end
+
+  def valid_github_payload?(_body_params, _req_headers) do
+    false
   end
 end
